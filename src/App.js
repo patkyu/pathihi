081157a9e4cc31be727b900fe0622b278265e38c
Patricia Mae Quitoriano <87472224+patkyu@users.noreply.github.com>
2023-07-19 22:36:05 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,605 @@
+import React from 'react';
+
+import Arrow1Image from './assets/images/6.png';
+
+import Image1Image from './assets/images/3.png';
+
+import Image2Image from './assets/images/4.png';
+
+import Image3Image from './assets/images/5.png';
+
+import Arrow2Image from './assets/images/7.png';
+
+import Arrow3Image from './assets/images/8.png';
+
+import {
+  styled
+} from '@mui/material/styles';
+
+import Q1549134132222912367Image from './assets/images/1.png';
+
+import Q1543134187508358722Image from './assets/images/2.png';
+
+const LandingPage1 = styled("div")({
+  backgroundColor: `rgba(255, 255, 255, 1)`,
+  display: `flex`,
+  position: `relative`,
+  isolation: `isolate`,
+  flexDirection: `row`,
+  width: `1920px`,
+  height: `4784px`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `0px`,
+  boxSizing: `border-box`,
+  overflow: `hidden`,
+});
+
+const Q1549134132222912367 = styled("div")({
+  backgroundImage: `url(${Q1549134132222912367Image})`,
+  backgroundPosition: `center`,
+  backgroundSize: `cover`,
+  backgroundRepeat: `no-repeat`,
+  width: `921px`,
+  height: `586px`,
+  position: `absolute`,
+  left: `476px`,
+  top: `962px`,
+});
+
+const Group1 = styled("div")({
+  display: `flex`,
+  position: `absolute`,
+  isolation: `isolate`,
+  flexDirection: `row`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `0px`,
+  boxSizing: `border-box`,
+  width: `158px`,
+  height: `68px`,
+  left: `230px`,
+  top: `122px`,
+});
+
+const PatKyu = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `400`,
+  fontSize: `36px`,
+  letterSpacing: `-1.8px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `122px`,
+  height: `49.62px`,
+  position: `absolute`,
+  left: `0px`,
+  top: `0px`,
+});
+
+const JuniorWebDeveloper = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `300`,
+  fontSize: `16px`,
+  letterSpacing: `-0.8px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `158px`,
+  height: `22.05px`,
+  position: `absolute`,
+  left: `0px`,
+  top: `46px`,
+});
+
+const PatriciaMaeDcQuitori = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `600`,
+  fontSize: `96px`,
+  letterSpacing: `-4.8px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+  width: `532px`,
+  height: `336px`,
+  position: `absolute`,
+  left: `230px`,
+  top: `398px`,
+});
+
+const JuniorWebDeveloper1 = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `300`,
+  fontSize: `24px`,
+  letterSpacing: `-1.2px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+  width: `384px`,
+  height: `38px`,
+  position: `absolute`,
+  left: `230px`,
+  top: `360px`,
+});
+
+const MeetPatATalentedAndDSpan1 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `300`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+});
+
+const MeetPatATalentedAndDSpan2 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `700`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+});
+
+const MeetPatATalentedAndDSpan3 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `300`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+});
+
+const MeetPatATalentedAndDSpan4 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `700`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+});
+
+const MeetPatATalentedAndDSpan5 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `300`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+});
+
+const MeetPatATalentedAndDSpan6 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `700`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+});
+
+const MeetPatATalentedAndDSpan7 = styled("span")({
+  whiteSpace: `pre-wrap`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `300`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+});
+
+const MeetPatATalentedAndD = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: "unset",
+  fontWeight: `400`,
+  fontSize: `32px`,
+  letterSpacing: `-1.6px`,
+  textDecoration: `none`,
+  lineHeight: `113.99999856948853%`,
+  textTransform: `none`,
+  width: `864px`,
+  height: `244px`,
+  position: `absolute`,
+  left: `815px`,
+  top: `398px`,
+});
+
+
+const Group2 = styled("div")({
+  display: `flex`,
+  position: `absolute`,
+  isolation: `isolate`,
+  flexDirection: `row`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `0px`,
+  boxSizing: `border-box`,
+  width: `493px`,
+  height: `68px`,
+  left: `1196px`,
+  top: `588px`,
+});
+
+
+
+
+const Q1543134187508358722 = styled("div")({
+  backgroundImage: `url(${Q1543134187508358722Image})`,
+  backgroundPosition: `center`,
+  backgroundSize: `cover`,
+  backgroundRepeat: `no-repeat`,
+  width: `1094px`,
+  height: `682px`,
+  position: `absolute`,
+  left: `585px`,
+  top: `814px`,
+});
+
+const Arrow1 = styled("img")({
+  height: `0px`,
+  width: `670px`,
+  position: `absolute`,
+  left: `565px`,
+  top: `1213px`,
+});
+
+const RecentProjects = styled("div")({
+  textAlign: `center`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(0, 0, 0, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `600`,
+  fontSize: `96px`,
+  letterSpacing: `-4.8px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `708px`,
+  height: `140px`,
+  position: `absolute`,
+  left: `606px`,
+  top: `1748px`,
+});
+
+const Image1 = styled("img")({
+  height: `748px`,
+  width: `1476px`,
+  objectFit: `cover`,
+  position: `absolute`,
+  left: `220px`,
+  top: `1940px`,
+});
+
+const Rectangle2 = styled("div")({
+  backgroundColor: `rgba(0, 0, 0, 1)`,
+  borderRadius: `92px`,
+  width: `1068px`,
+  height: `178px`,
+  position: `absolute`,
+  left: `424px`,
+  top: `2616px`,
+});
+
+const BrainFoodLanguageLea = styled("div")({
+  textAlign: `center`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `400`,
+  fontSize: `40px`,
+  letterSpacing: `-2px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `680px`,
+  height: `69px`,
+  position: `absolute`,
+  left: `618px`,
+  top: `2670px`,
+});
+
+const Rectangle3 = styled("div")({
+  backgroundColor: `rgba(0, 0, 0, 1)`,
+  borderRadius: `92px`,
+  width: `880px`,
+  height: `178px`,
+  position: `absolute`,
+  left: `816px`,
+  top: `3135px`,
+});
+
+const PhilHealthReMastered = styled("div")({
+  textAlign: `center`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `400`,
+  fontSize: `40px`,
+  letterSpacing: `-2px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `680px`,
+  height: `69px`,
+  position: `absolute`,
+  left: `958px`,
+  top: `3189px`,
+});
+
+const Image2 = styled("img")({
+  height: `724px`,
+  width: `724px`,
+  objectFit: `cover`,
+  position: `absolute`,
+  left: `220px`,
+  top: `2862px`,
+});
+
+const Image3 = styled("img")({
+  height: `691px`,
+  width: `1476px`,
+  objectFit: `cover`,
+  position: `absolute`,
+  left: `220px`,
+  top: `3709px`,
+});
+
+const Rectangle4 = styled("div")({
+  backgroundColor: `rgba(0, 0, 0, 1)`,
+  borderRadius: `92px`,
+  width: `1068px`,
+  height: `178px`,
+  position: `absolute`,
+  left: `426px`,
+  top: `4311px`,
+});
+
+const BlendsCoffeeShopWebs = styled("div")({
+  textAlign: `center`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `400`,
+  fontSize: `40px`,
+  letterSpacing: `-2px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `680px`,
+  height: `69px`,
+  position: `absolute`,
+  left: `620px`,
+  top: `4365px`,
+});
+
+const Arrow2 = styled("img")({
+  height: `0px`,
+  width: `721px`,
+  position: `absolute`,
+  left: `975px`,
+  top: `2995px`,
+});
+
+const Arrow3 = styled("img")({
+  height: `0px`,
+  width: `721px`,
+  position: `absolute`,
+  left: `2417px`,
+  top: `3452px`,
+});
+
+const Rectangle5 = styled("div")({
+  backgroundColor: `rgba(0, 0, 0, 1)`,
+  width: `1920px`,
+  height: `172px`,
+  position: `absolute`,
+  left: `0px`,
+  top: `4612px`,
+});
+
+const Q09270585088WorkPatk = styled("div")({
+  textAlign: `right`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `400`,
+  fontSize: `16px`,
+  letterSpacing: `-0.8px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `688px`,
+  height: `52px`,
+  position: `absolute`,
+  left: `1008px`,
+  top: `4672px`,
+});
+
+const Group3 = styled("div")({
+  display: `flex`,
+  position: `absolute`,
+  isolation: `isolate`,
+  flexDirection: `row`,
+  justifyContent: `flex-start`,
+  alignItems: `flex-start`,
+  padding: `0px`,
+  boxSizing: `border-box`,
+  width: `158px`,
+  height: `68px`,
+  left: `220px`,
+  top: `4664px`,
+});
+
+const PatKyu1 = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `400`,
+  fontSize: `36px`,
+  letterSpacing: `-1.8px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `122px`,
+  height: `49.62px`,
+  position: `absolute`,
+  left: `0px`,
+  top: `0px`,
+});
+
+const JuniorWebDeveloper2 = styled("div")({
+  textAlign: `left`,
+  whiteSpace: `pre-wrap`,
+  fontSynthesis: `none`,
+  color: `rgba(255, 255, 255, 1)`,
+  fontStyle: `normal`,
+  fontFamily: `Poppins`,
+  fontWeight: `300`,
+  fontSize: `16px`,
+  letterSpacing: `-0.8px`,
+  textDecoration: `none`,
+  textTransform: `none`,
+  width: `158px`,
+  height: `22.05px`,
+  position: `absolute`,
+  left: `0px`,
+  top: `46px`,
+});
+
+
+function LandingPage() {
+  return (
+    <LandingPage1>
+      <Q1549134132222912367>
+      </Q1549134132222912367>
+      <Group1>
+        <PatKyu>
+          {`Pat Kyu`}
+        </PatKyu>
+        <JuniorWebDeveloper>
+          {`Junior Web Developer`}
+        </JuniorWebDeveloper>
+      </Group1>
+      <PatriciaMaeDcQuitori>
+        {`Patricia Mae DC. Quitoriano`}
+      </PatriciaMaeDcQuitori>
+      <JuniorWebDeveloper1>
+        {`Junior Web Developer`}
+      </JuniorWebDeveloper1>
+      <MeetPatATalentedAndD>
+        <MeetPatATalentedAndDSpan1 >{`Meet `}</MeetPatATalentedAndDSpan1>
+        <MeetPatATalentedAndDSpan2 >{`Pat`}</MeetPatATalentedAndDSpan2>
+        <MeetPatATalentedAndDSpan3 >{`, a talented and driven `}</MeetPatATalentedAndDSpan3>
+        <MeetPatATalentedAndDSpan4 >{`Junior Front-End Web Developer`}</MeetPatATalentedAndDSpan4>
+        <MeetPatATalentedAndDSpan5 >{` with a passion for creating exceptional 
+user experiences. With a solid foundation in 
+`}</MeetPatATalentedAndDSpan5>
+        <MeetPatATalentedAndDSpan6 >{`HTML, CSS, and React.js`}</MeetPatATalentedAndDSpan6>
+        <MeetPatATalentedAndDSpan7 >{`, they excel in 
+turning design concepts into 
+interactive and visually 
+appealing websites.`}</MeetPatATalentedAndDSpan7>
+      </MeetPatATalentedAndD>
+      <Group2>
+      </Group2>
+      <Q1543134187508358722>
+      </Q1543134187508358722>
+      <Arrow1 src={Arrow1Image} loading='lazy' alt={"Arrow 1"}/>
+      <RecentProjects>
+        {`Recent Projects`}
+      </RecentProjects>
+      <Image1 src={Image1Image} loading='lazy' alt={"image 1"}/>
+      <Rectangle2>
+      </Rectangle2>
+      <BrainFoodLanguageLea>
+        {`Brain Food: Language Learning App`}
+      </BrainFoodLanguageLea>
+      <Rectangle3>
+      </Rectangle3>
+      <PhilHealthReMastered>
+        {`PhilHealth ReMastered`}
+      </PhilHealthReMastered>
+      <Image2 src={Image2Image} loading='lazy' alt={"image 2"}/>
+      <Image3 src={Image3Image} loading='lazy' alt={"image 3"}/>
+      <Rectangle4>
+      </Rectangle4>
+      <BlendsCoffeeShopWebs>
+        {`Blends: Coffee Shop Website`}
+      </BlendsCoffeeShopWebs>
+      <Arrow2 src={Arrow2Image} loading='lazy' alt={"Arrow 2"}/>
+      <Arrow3 src={Arrow3Image} loading='lazy' alt={"Arrow 3"}/>
+      <Rectangle5>
+      </Rectangle5>
+      <Q09270585088WorkPatk>
+        {`09270585088
+work.patkyu@gmail.com`}
+      </Q09270585088WorkPatk>
+      <Group3>
+        <PatKyu1>
+          {`Pat Kyu`}
+        </PatKyu1>
+        <JuniorWebDeveloper2>
+          {`Junior Web Developer`}
+        </JuniorWebDeveloper2>
+      </Group3>
+    </LandingPage1>);
+
+  }
+
+export default LandingPage;
+
+  
